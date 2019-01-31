8955080eb84c997cffc17e64bb37bc4caced0dfe
yeshoham <yeshoham@cisco.com>
2019-01-31 20:37:36 +0200

    some styling and refractoring for ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,11 +4,15 @@ import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = (props) => (
-    <div>
-        Viewing {props.expensesCount} {props.expensesCount == 1 ? 'expense' : 'expenses'} totalling ₪{numeral(props.expensesTotal / 100).format('0,0.00')}
-    </div>
-);
+export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
+    const expenseWord = expensesCount == 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('0,0.00');
+    return (
+        <div>
+            <h1>Viewing {expensesCount} {expenseWord} totalling ₪{formattedExpensesTotal}</h1>
+        </div>
+    )
+};
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
