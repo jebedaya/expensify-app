e0354cd9a5a9094325e00ae7e7bfb7693aa81344
yeshoham <yeshoham@cisco.com>
2019-02-14 20:49:41 +0200

    Add Expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -12,7 +12,7 @@ export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>₪{formattedExpensesTotal}</span></h1>
-                <div className="page_header__actions">
+                <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
             </div>
